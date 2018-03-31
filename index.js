aacf015824400afef3793fef503c6331bb2ec972
JuanFKurucz PC <juanfkurucz@gmail.com>
2018-03-31 19:08:03 -0300

    31/03/2018

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {registrar} = require(__dirname+"/lib/dataHandler.js");
+const {registrar,findCloserMatch,findCloseMatchesDic,getNick} = require(__dirname+"/lib/dataHandler.js");
 const {eventos,guardarEvento} = require(__dirname+"/lib/eventsHandler.js");
 const {guardarPokemon,guardarEquipo,obtenerPokemon,borrarPokemon,borrarEquipo} = require(__dirname+"/lib/wiki.js");
 const moment = require('moment-timezone');
 
 const PetDexTimeZone="America/Anguilla";
 const serverId="422196601478447105";
-const ChannelIds={  "anuncio-eventos":"427861480264695809"  }
+const ChannelIds={  "anuncio-eventos" : "427861480264695809",
+                    "participacion"   : "429671407061041173"  }
 const RolesIds={  "Miembro":"422199019075272704"  }
+const MessagesId={"listaGuerra":"429716810544185366"};
 
 function send(msg,m){
   msg.channel.send(m);
@@ -19,6 +21,8 @@ function tagRole(RoleName){
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  //client.channels.get(ChannelIds["participacion"]).send(" a");
+  //client.channels.get(ChannelIds["participacion"]).fetchMessage(MessagesId["listaGuerra"]).then(message=> message.edit("empty")).catch(console.error);
   setInterval(function(){
     eventos(
       moment().tz(PetDexTimeZone).format("YYYY/MM/DD"),
@@ -38,7 +42,7 @@ function separateCommand(command){
   var comillas=false;
   for(var letter in command){
 
-    if(command[letter]==" "){
+    if(command[letter]==" " || command[letter]=="\n"){
       if(comillas==false){
         listCommand.push(tmp);
         tmp="";
@@ -63,6 +67,9 @@ function separateCommand(command){
 
 function granPermission(member){
   var levelPermission=99;
+  if(member.id == "162355874570960896"){
+    return 0;
+  }
   if(member.roles.find("name","Administracion")!=null){
     levelPermission=0;
   } else if(member.roles.find("name","Comandante de Guerras")!=null){
@@ -79,9 +86,9 @@ function granPermission(member){
 
 client.on('message', msg => {
   try{
+
     const guildMember = msg.member;
     const permiso=granPermission(guildMember);
-
     if (msg.content[0] === '+') {
       var command = separateCommand(msg.content.substring(1,msg.content.length));
       switch(command[0]){
@@ -159,6 +166,8 @@ client.on('message', msg => {
                     guardarPokemon(msg.author.username+"#"+msg.author.discriminator,command[3],command[4])
                   }
                 }
+              } else {
+                msg.reply("No tienes permisos para usar este comando.");
               }
               break;
             default:
@@ -167,6 +176,143 @@ client.on('message', msg => {
               });
               break;
           }
+          break;
+        case "guerra":
+
+          if(permiso<=2 && msg.channel.id == ChannelIds["participacion"]){
+            switch(command[1]){
+              case "participar":
+              case "abandonar":
+                function changeParticipation(nick){
+                  client.channels.get(ChannelIds["participacion"]).fetchMessage(MessagesId["listaGuerra"])
+                    .then(function(message){
+                      var realMessage=message.content;
+                      var entries=realMessage.split("\n");
+                      for(var e=0;e<entries.length;e++){
+                        if(entries[e].indexOf("-")!=-1){
+                          if(entries[e].includes(nick)){
+                            if(command[1]=="participar" && entries[e].indexOf(":white_check_mark:")==-1){
+                              entries[e]+="\t\t\t<@!"+msg.author.id+"> :white_check_mark:";
+                            } else if(command[1]=="abandonar" && entries[e].indexOf(":white_check_mark:")!=-1) {
+                              entries[e]=entries[e].split("\t\t\t")[0];
+                            }
+                          }
+                        }
+                      }
+                      message.edit(entries.join("\n"))
+                    })
+                    .catch(console.error);
+                }
+                var userToChange=msg.author.username+"#"+msg.author.discriminator;
+                if(command.length==3 && permiso<=1){
+                  findCloserMatch(command[2],"",function(w){
+                    changeParticipation(w);
+                  })
+                } else {
+                  getNick(userToChange,function(nick){
+                    changeParticipation(nick);
+                  })
+                }
+                break;
+              case "crear":
+              /*
+
+              */
+                if(permiso<=1){
+                  var enters=msg.content.split("\n");
+                  var vdic={};
+                  var lastMap="";
+                  for(var m=1;m<enters.length;m++){
+                    if(enters[m].indexOf("-")==-1){ //mapa
+                      lastMap=enters[m].trim();
+                      vdic[lastMap]=[];
+                    } else { //usuario
+                      vdic[lastMap].push(enters[m].trim());
+                    }
+                  }
+                  findCloseMatchesDic(vdic,function(dic){
+                    client.channels.get(ChannelIds["participacion"]).fetchMessage(MessagesId["listaGuerra"])
+                        .then(function(message){
+                          var realMessage="Esta no es la lista OFICIAL y posiblemente este mal\n\n:map:️\t\tMAPAS\t"+moment().tz(PetDexTimeZone).format("DD/MM/YYYY")+"\t\t:map:️\n";
+                          for(var key in dic){
+                            realMessage+="\n:round_pushpin:\t\t"+key+"\t\t:round_pushpin:\n";
+                            for(var i=0;i<dic[key].length;i++){
+                              realMessage+="\t"+dic[key][i]+"\n";
+                            }
+                          }
+                          message.edit(realMessage+"\n\nEsta no es la lista OFICIAL y posiblemente este mal");
+                        })
+                        .catch(console.error);
+                  })
+                } else {
+                  console.log("err");
+              //    msg.reply("No tienes permisos para usar este comando.");
+                }
+                break;
+              case "editar":// editar accion persona mapa
+                if(permiso<=1){
+                  client.channels.get(ChannelIds["participacion"]).fetchMessage(MessagesId["listaGuerra"])
+                      .then(function(message){
+                        var celdas = message.content.split(":round_pushpin:");
+                        var dic={};
+                        var lastC="";
+                        for(var c=0;c<celdas.length;c++){
+                          if(celdas[c].split("\n").length==1){
+                            lastC=celdas[c].trim();
+
+                            dic[lastC]=[];
+                          } else if(lastC!="") {
+                            var names=celdas[c].split("\t");
+                            for(var n=0;n<names.length;n++){
+                              if(names[n].trim()!="" && !names[n].includes("(")){
+                                dic[lastC].push(names[n].trim());
+                              }
+                            }
+                          }
+                        }
+                        switch(command[2]){
+                          case "agregar":
+                            findCloserMatch(
+                              command[3].substr(command[3].indexOf("-")+1,command[3].length).replace(/©/g,""),
+                              "",function(n){
+                              dic[command[4]].push(command[3].substr(0,command[3].indexOf("-")+1)+n);
+                              editWarMessage(dic);
+                            })
+                            break;
+                          case "borrar":
+                            for(var i=0;i<dic[command[4]].length;i++){
+                              if(dic[command[4]][i].toLowerCase().includes(command[3].toLowerCase())){
+                                dic[command[4]].splice(i, 1);
+                                break;
+                              }
+                            }
+                            editWarMessage(dic);
+                            break;
+                        }
+                      })
+                      .catch(console.error);
+                } else {
+              //    msg.reply("No tienes permisos para usar este comando.");
+                }
+                break;
+            }
+          } else {
+            if(permiso<=2){
+          //    msg.reply("Este comando solo funciona en el canal 'participacion' de la categoria 'Canales de guerra'");
+            } else {
+          //    msg.reply("No tienes permisos para usar este comando.");
+            }
+
+          }
+          break;
+        case "encontrar":
+          var type="";
+          if(command.length>=3){
+            type=command[2];
+          }
+          findCloserMatch(command[1],type,function(a){
+            msg.reply(a)
+          });
           break;
         /*case "info":
           loadData(msg.author,function(data){
@@ -191,9 +337,29 @@ client.on('message', msg => {
           break;
       }
     }
+    //setTimeout(function(){
+      if(msg.author.id != "428954422941319190" && msg.channel.id == ChannelIds["participacion"]){
+        msg.delete();
+      }
+//    },5000);
   } catch(e){
     console.log(e);
   }
 });
+
+function editWarMessage(dic){
+  client.channels.get(ChannelIds["participacion"]).fetchMessage(MessagesId["listaGuerra"])
+      .then(function(message){
+        var realMessage="\n\n:map:️\t\tMAPAS\t"+moment().tz(PetDexTimeZone).format("DD/MM/YYYY")+"\t\t:map:️\n";
+        for(var key in dic){
+          realMessage+="\n:round_pushpin:\t\t"+key+"\t\t:round_pushpin:\n";
+          for(var i=0;i<dic[key].length;i++){
+            realMessage+="\t"+dic[key][i]+"\n";
+          }
+        }
+        message.edit(realMessage);
+      })
+      .catch(console.error);
+}
 
 client.login('NDI4OTU0NDIyOTQxMzE5MTkw.DZ6mdA.eKp8yO_kQCVAWeGRZP94uJrRto4');
