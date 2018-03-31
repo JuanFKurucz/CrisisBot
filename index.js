117134c7ffa204b81403b17ba3707a4db00f9c3d
JuanFKurucz PC <juanfkurucz@gmail.com>
2018-03-31 05:14:37 -0300

    31/03/2018

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {registrar} = require("./dataHandler.js");
-const {eventos,guardarEvento} = require("./eventsHandler.js");
-const {guardarPokemon,guardarEquipo,obtenerPokemon} = require("./wiki.js");
+const {registrar} = require(__dirname+"/lib/dataHandler.js");
+const {eventos,guardarEvento} = require(__dirname+"/lib/eventsHandler.js");
+const {guardarPokemon,guardarEquipo,obtenerPokemon,borrarPokemon,borrarEquipo} = require(__dirname+"/lib/wiki.js");
 const moment = require('moment-timezone');
 
 const PetDexTimeZone="America/Anguilla";
@@ -61,84 +61,138 @@ function separateCommand(command){
   return listCommand;
 }
 
+function granPermission(member){
+  var levelPermission=99;
+  if(member.roles.find("name","Administracion")!=null){
+    levelPermission=0;
+  } else if(member.roles.find("name","Comandante de Guerras")!=null){
+    levelPermission=1;
+  } else if(member.roles.find("name","Miembro")!=null){
+    levelPermission=2;
+  } else if(member.roles.find("name","Miembro Pendiente")!=null){
+    levelPermission=3;
+  } else {
+    levelPermission=4;
+  }
+  return levelPermission;
+}
 
 client.on('message', msg => {
-  const guildMember = msg.member;
-  if (msg.content[0] === '+') {
-    var command = separateCommand(msg.content.substring(1,msg.content.length));
-    switch(command[0]){
-      case "registrar":
-        if(command.length<=1){
-          msg.reply("El comando necesita que ingreses tu nick especifico");
-        } else if(!command[1].includes("©")){
-          msg.reply("Debido a las reglas el nick inGame tiene que incluir el caracter ©");
-        } else {
-          registrar(msg.author,command[1],function(er,s){
-            msg.reply(er);
-            if(s){
-              guildMember.addRole(msg.guild.roles.find("name", "Miembro Pendiente"));
+  try{
+    const guildMember = msg.member;
+    const permiso=granPermission(guildMember);
+
+    if (msg.content[0] === '+') {
+      var command = separateCommand(msg.content.substring(1,msg.content.length));
+      switch(command[0]){
+        case "registrar":
+          if(permiso==4){
+            if(command.length<=1){
+              msg.reply("El comando necesita que ingreses tu nick especifico");
+            } else if(!command[1].includes("©")){
+              msg.reply("Debido a las reglas el nick inGame tiene que incluir el caracter ©");
+            } else {
+              registrar(msg.author,command[1],function(er,s){
+                msg.reply(er);
+                if(s){
+                  guildMember.addRole(msg.guild.roles.find("name", "Miembro Pendiente"));
+                }
+              });
             }
-          });
-        }
-        break;
-      case "recordar":
-        if(command.length<=3){
-          msg.reply("El comando necesita que ingreses: El mensaje a recordar y la hora (hora del juego), y el dia de la semana (1/7, usar 0 para todos los dias y -1 para que solo se ejecute una vez)\nEjemplo: +recordar 'Fiesta de Pokemons' 17:51 3");
-        }
-        var dia=-1;
-        if(!isNaN(command[3])){
-          dia=command[3];
-        }
-        guardarEvento(
-          {
-            "text":command[1],
-            "time":command[2],
-            "last":"",
-            "when":dia
+          } else if(permiso==3){
+            msg.reply("Tu aplicacion a Miembros esta en espera, contacta a un administrador");
+          } else {
+            msg.reply("Ya eres Miembro");
           }
-        );
-        break;
-      case "wiki":
-        switch(command[1]){
-          case "agregar":
-            if(command[2]=="equipo"){
-              if(command.length!=6){
-                msg.reply("Se deben ingresar 3 pokemons")
-              } else {
-                guardarEquipo(msg.author.username+"#"+msg.author.discriminator,command[3],command[4],command[5])
-              }
-            } else if(command[2]=="pokemon"){
-              guardarPokemon(msg.author.username+"#"+msg.author.discriminator,command[3],command[4])
+          break;
+        case "echar":
+          if(permiso<=1){
+
+          } else {
+            msg.reply("No tienes permiso para echar usuarios.");
+          }
+          break;
+        case "recordar":
+          if(permiso<=2){
+            if(command.length<3){
+              msg.reply("El comando necesita que ingreses: El mensaje a recordar y la hora (hora del juego), y el dia de la semana (1/7, usar 0 para todos los dias y -1 para que solo se ejecute una vez)\nEjemplo: +recordar 'Fiesta de Pokemons' 17:51 3");
             }
-            break;
-          case "borrar":
-            break;
-          default:
-            obtenerPokemon(command[2],function(data){
-              msg.reply(data);
-            });
-            break;
-        }
-        break;
-      /*case "info":
-        loadData(msg.author,function(data){
-          //msg.send('Perfil de '+data.Nombre+":\nNivel:"+data.Nivel+"\nXP:"+data.XP);
-        });
-        break;*/
-      /*case "mapa":
-        var a=new Discord.RichEmbed()
-          .setTitle("Mapa")
-          .setAuthor("Tu",client.user.avatarURL)
-          .setColor(0x00AE86)
-          .setFooter("@Hello",client.user.avatarURL)
-          .setImage("https://orig00.deviantart.net/9bb7/f/2011/309/1/0/foregon_town_my_first_pokemon_map_by_elish_56-d4f5ik0.jpg");
-        send(msg,a);
-        //send(msg,'123456789.123456789.123456789.123456789.123456789.123456789.123456789.123456789');
-        break;*/
-      default:
-        msg.reply('El comando no existe');
-        break;
+            var dia=-1;
+            if(!isNaN(command[3])){
+              dia=command[3];
+            }
+            if(dia==0 && permiso >=2){
+              msg.reply("No tienes permiso para activar un recordatorio diario.");
+            } else {
+              guardarEvento(
+                {
+                  "text":command[1],
+                  "time":command[2],
+                  "last":"",
+                  "when":dia
+                }
+              );
+            }
+          } else {
+            msg.reply("No tienes permisos para usar este comando.");
+          }
+          break;
+        case "wiki":
+          switch(command[1]){
+            case "agregar":
+            case "borrar":
+              if(permiso<=2){
+                if(command[2]=="equipo"){
+                  if(command.length!=6){
+                    msg.reply("Se deben ingresar 3 pokemons")
+                  } else {
+                    if(command[1]=="borrar"){
+                      borrarEquipo(msg.author.username+"#"+msg.author.discriminator,command[3],command[4],command[5])
+                    } else {
+                      guardarEquipo(msg.author.username+"#"+msg.author.discriminator,command[3],command[4],command[5])
+                    }
+                  }
+                } else if(command[2]=="pokemon"){
+                  if(command[1]=="borrar"){
+                    borrarPokemon(msg.author.username+"#"+msg.author.discriminator,command[3],command[4],command[5])
+                  } else {
+                    guardarPokemon(msg.author.username+"#"+msg.author.discriminator,command[3],command[4])
+                  }
+                }
+              }
+              break;
+            default:
+              obtenerPokemon(command[1],function(data){
+                msg.reply(data.replace(/:pokeball:/g,client.emojis.find("name", "pokeball")));
+              });
+              break;
+          }
+          break;
+        /*case "info":
+          loadData(msg.author,function(data){
+            //msg.send('Perfil de '+data.Nombre+":\nNivel:"+data.Nivel+"\nXP:"+data.XP);
+          });
+          break;*/
+        /*case "mapa":
+          var a=new Discord.RichEmbed()
+            .setTitle("Mapa")
+            .setAuthor("Tu",client.user.avatarURL)
+            .setColor(0x00AE86)
+            .setFooter("@Hello",client.user.avatarURL)
+            .setImage("https://orig00.deviantart.net/9bb7/f/2011/309/1/0/foregon_town_my_first_pokemon_map_by_elish_56-d4f5ik0.jpg");
+          send(msg,a);
+          //send(msg,'123456789.123456789.123456789.123456789.123456789.123456789.123456789.123456789');
+          break;*/
+        case "hora":
+          msg.reply('Hora del servidor: '+moment().tz(PetDexTimeZone).format("HH:mm"));
+          break;
+        default:
+          msg.reply('El comando no existe');
+          break;
+      }
     }
+  } catch(e){
+    console.log(e);
   }
 });
 
